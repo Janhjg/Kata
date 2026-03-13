a072c2361d735ffc212312b3153488fe7152d3c2
Janhjg <janhjanh93@gmail.com>
2026-03-13 10:53:03 +0100

    anadido todas las katas

--- password_validator/services.py
+++ password_validator/services.py
@@ -5,11 +5,34 @@ class PasswordErrorShort(Exception):
 class PasswordNoNumberError(Exception):
     def __init__(self):
         super().__init__("La contrasena debe contener al menos un numero.")
+        
+class PasswordNoCaracterError(Exception):
+    def __init__(self):
+        super().__init__("La contrasena debe contener al menos un caracter.")
 
+class PasswordNoMayusError(Exception):
+    def __init__(self):
+        super().__init__("La contrasena debe contener al menos una letra en mayuscula.")
+
+class PasswordNoMinusError(Exception):
+    def __init__(self):
+        super().__init__("La contrasena debe contener al menos una letra en minuscula.")
+
+class PasswordNoSpecialCharError(Exception):
+    def __init__(self):
+        super().__init__("La contrasena debe contener al menos un caracter especial.")
 
 def validate_password(password):
     if len(password) < 8:
         raise PasswordErrorShort()
     if not any(c.isdigit() for c in password):
         raise PasswordNoNumberError()
+    if not any(c.isalpha() for c in password):
+        raise PasswordNoCaracterError()
+    if not any(c.isupper() for c in password):
+        raise PasswordNoMayusError()
+    if not any(c.islower() for c in password):
+        raise PasswordNoMinusError()
+    if not any(not c.isalnum() for c in password):
+        raise PasswordNoSpecialCharError()
     return True
--- password_validator/test_password_validator.py
+++ password_validator/test_password_validator.py
@@ -9,5 +9,21 @@ def test_password_sin_numero():
     with pytest.raises(PasswordNoNumberError):
         validate_password("abcdefgh")
 
+def test_password_sin_caracter():
+    with pytest.raises(PasswordNoCaracterError):
+        validate_password("12345678")
+        
+def test_password_sin_mayus():
+    with pytest.raises(PasswordNoMayusError):
+        validate_password("1234nnnn")
+        
+def test_password_sin_minus():
+    with pytest.raises(PasswordNoMinusError):
+        validate_password("1234AAAA")
+        
+def test_password_sin_especial():
+    with pytest.raises(PasswordNoSpecialCharError):
+        validate_password("Abcdefg1")
+
 def test_password_valida():
-    assert validate_password("abcdefg1") == True
+    assert validate_password("Abcdefg1!") == True
